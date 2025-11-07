5f93c2cc1555b0c5c0efefe385bb5ac38d35ad15
Mỹ Hân Lưu <luum@purdue.edu>
2025-11-06 23:58:48 -0500

    chardisp init, spi_cmd, spi_data

--- display.c
+++ display.c
@@ -8,11 +8,30 @@
 
 
 
-void display_init_spi() {
+void init_chardisp_pins() {
     // fill in    
     gpio_set_function(SPI_TFT_SCK,GPIO_FUNC_SPI);
     gpio_set_function(SPI_TFT_TX,GPIO_FUNC_SPI);
     gpio_set_function(SPI_TFT_CSn,GPIO_FUNC_SPI);
-    spi_init (spi1, 125000); // Always enable DREQ signals -- harmless if DMA is not listening
-    spi_set_format(spi1,16,0,0,SPI_MSB_FIRST);
+    spi_init (spi0, 10000); // Always enable DREQ signals -- harmless if DMA is not listening
+    //low clock frequency to make sure the display has enough time to process commands sent to it
+    // will increase if transmitted signal requires higher freq
+    spi_set_format(spi0,9,0,0,SPI_MSB_FIRST);
+    //9 bits for LCD - verify
+}
+
+void send_spi_cmd(spi_inst_t* spi, uint16_t value){
+    while (spi_is_busy(spi0));
+    spi_write16_blocking(spi0,&value,1);
+}
+void send_spi_data(spi_inst_t* spi, uint16_t value){
+    send_spi_cmd(spi0,value|0x100);
+}
+
+void cd_init(){
+    sleep_ms(1);
+    //Function Set command 0b0000101100
+     //Display On/Off command 0b0000001100
+    //Clear Display 0b0000000001
+     //Entry Mode Set command 0b0000000110
 }
