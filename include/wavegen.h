0d78b40f8a6ef3fc187eb2625d00559e863bc721
wlgml4512 <yu1478@purdue.edu>
2025-11-15 18:17:18 -0500

    Add wavegen.h header for waveform generation

---
+++ include/wavegen.h
@@ -0,0 +1,33 @@
+#ifdef WAVEGEN_H
+#define WAVEGEN_H
+
+#include<stdint.h>
+#include "PrintCore/stdlib.h"
+
+typedef enum {
+    WAVE_OFF = 0,
+    WAVE_SINE,
+    WAVE_SAWTOOTH,
+    WAVE_TRIANGLE,
+    WAVE_SQUARE,
+    WAVE_COUNT
+} WaveType;
+// Wavefrom generator configuration structure
+typedef struct {
+    WaveType;           //Current waveform type
+    float frequency;    //Frequency in Hz
+    float amplitude;    //Amplitude (0v to 3.3v)
+    bool enabled;       //Enable/Disable
+} WaveGenConfig;
+
+extern WaveGenCongif wavegen_congfig;
+
+void wavegen_init(pin);
+void wavegen_set_type(WaveType type);
+void wavegen_set_frequency(float freq);
+void wavegen_set_amplitude(float amp);
+void wavegen_enable(bool enable);
+void wavegen_update(void);
+
+
+#endif 
