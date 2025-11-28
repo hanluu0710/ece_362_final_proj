ff6aec5dfdb4002207d67699ebf3f08fa306c43d
Mỹ Hân Lưu <luum@purdue.edu>
2025-11-28 03:26:25 -0500

    done integrating shift and scale Y. done integrating scale X. DITCH shift X

--- CODE/src/button_and_rotary.c
+++ CODE/src/button_and_rotary.c
@@ -1,266 +1,489 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include "buttons.h"
 
-#define BUTTON_MODE 3
-#define BUTTON_CAPTURE 4
+// //global var
+// static bool last_mode_state = false;
+// static bool last_capture_state = false;
+// static bool last_x_state = false;
+// static bool last_y_state = false;
+// extern bool pause;
 
-#define X_CLK 5
-#define X_DT 6
-#define X_SW 7
+// #define WAVE_COUNT 5
 
-#define Y_CLK 8
-#define Y_DT 9
-#define Y_SW 10
+// // scale settings of the rotary
+// int x_scale = 0;
+// int y_scale = 0;
+// bool x_fast = false;
+// bool y_fast = false;
 
-// scale settings of the rotary
-int x_scale = 0;
-int y_scale = 0;
-bool x_fast = false;
-bool y_fast = false;
+void init_gpio()
+{
+    // Initializing BUTTON_MODE
+    gpio_init(BUTTON_MODE);
+    gpio_set_dir(BUTTON_MODE, false);
 
+    // Initializing BUTTON_CAPTURE
+    gpio_init(BUTTON_CAPTURE);
+    gpio_set_dir(BUTTON_CAPTURE, false);
 
-typedef enum {
-    WAVE_OFF,
-    WAVE_SINE,
-    WAVE_SAWTOOTH,
-    WAVE_TRIANGLE,
-    WAVE_SQUARE,
-    WAVE_COUNT
-} wave_type;
+    // Initializing X encoder
+    gpio_init(X_CLK);
+    gpio_set_dir(X_CLK, GPIO_IN);
+    gpio_pull_up(X_CLK);
 
-void wave_output(wave_type wave) {
-    switch(wave) {
-        case WAVE_OFF:
-            printf("Wave OFF\n");
-            break;
-        case WAVE_SINE:
-            printf("Wave SINE\n");
-            break;
-        case WAVE_SAWTOOTH:
-            printf("Wave SAW\n");
-            break;
-        case WAVE_TRIANGLE:
-            printf("Wave TRIANGLE\n");
-            break;
-        case WAVE_SQUARE:
-            printf("Wave SQUARE\n");
-            break;
-        default:
-            break;
-    }
+    gpio_init(X_DT);
+    gpio_set_dir(X_DT, GPIO_IN);
+    gpio_pull_up(X_DT);
+
+    gpio_init(X_SW);
+    gpio_set_dir(X_SW, GPIO_IN);
+    gpio_pull_up(X_SW);
+
+    // Initializing Y encoder
+    gpio_init(Y_CLK);
+    gpio_set_dir(Y_CLK, GPIO_IN);
+    gpio_pull_up(Y_CLK);
+
+    gpio_init(Y_DT);
+    gpio_set_dir(Y_DT, GPIO_IN);
+    gpio_pull_up(Y_DT);
+
+    gpio_init(Y_SW);
+    gpio_set_dir(Y_SW, GPIO_IN);
+    gpio_pull_up(Y_SW);
+
+    printf("Wave Generator Ready\n");
 }
 
-bool check_mode_button() {
-    static bool last_state = false;
-    bool current_state = gpio_get(BUTTON_MODE);
-
-    if (!last_state && current_state) {
-        last_state = current_state;
-        sleep_ms(100);
-        return true;
-    }
-    last_state = current_state;
-    return false;
-}
-
-bool check_capture_button() {
-    static bool last_state = false;
-    bool current_state = gpio_get(BUTTON_CAPTURE);
-
-    if (!last_state && current_state) {
-        last_state = current_state;
-        sleep_ms(100);
-        return true;
-    }
-    last_state = current_state;
-    return false;
-}
-
-// read the x scale encoder
-int read_encoder_x() {
-    static int last_clk_x = 0;
-    int clk = gpio_get(X_CLK);
-    int dt = gpio_get(X_DT);
-    int result = 0;
-
-    if (clk != last_clk_x) {
-        result = (dt != clk) ? 1 : -1;
-    }
-    last_clk_x = clk;
-    return result;
-}
-
-// y scale encoder reading
-int read_encoder_y() {
-    static int last_clk = 0;
-    int clk = gpio_get(Y_CLK);
-    int dt = gpio_get(Y_DT);
-    int result = 0;
-
-    if (clk != last_clk) {
-        result = (dt != clk) ? 1 : -1;
-    }
-    last_clk = clk;
-    return result;
-}
-
-// check when x rotray button is pressed
-bool last_x_state = false; // global var to store the state
-
-bool check_x_button() {
-    bool current = gpio_get(X_SW);
-
-    if (!last_x_state && current) {
-        sleep_ms(100);
-        last_x_state = current;
-        return true;
-    }
-    last_x_state = current;
-    return false;
-}
-
-// this is check when y button is pressed or not
-bool last_y_state = false; 
-
-bool check_y_button() {
-    bool current = gpio_get(Y_SW);
-
-    if (!last_y_state && current) {
-        sleep_ms(100);
-        last_y_state = current;
-        return true;
-    }
-    last_y_state = current;
-    return false;
-}
-
-// =========================================================== //
-
-// int main() {
-//     stdio_init_all();
-
-//     // Initializing BUTTON_MODE
-//     gpio_init(BUTTON_MODE);
-//     gpio_set_dir(BUTTON_MODE, false);
-
-//     // Initializing BUTTON_CAPTURE
-//     gpio_init(BUTTON_CAPTURE);
-//     gpio_set_dir(BUTTON_CAPTURE, false); 
-
-//     // Initializing X encoder
-//     gpio_init(X_CLK); 
-//     gpio_set_dir(X_CLK, GPIO_IN);
-//     gpio_pull_up(X_CLK);
-
-//     gpio_init(X_DT);  
-//     gpio_set_dir(X_DT, GPIO_IN);
-//     gpio_pull_up(X_DT);
-
-//     gpio_init(X_SW);  
-//     gpio_set_dir(X_SW, GPIO_IN);
-//     gpio_pull_up(X_SW);
-
-//     // Initializing Y encoder
-//     gpio_init(Y_CLK); 
-//     gpio_set_dir(Y_CLK, GPIO_IN);
-//     gpio_pull_up(Y_CLK);
-
-//     gpio_init(Y_DT);  
-//     gpio_set_dir(Y_DT, GPIO_IN);
-//     gpio_pull_up(Y_DT);
-
-//     gpio_init(Y_SW);  
-//     gpio_set_dir(Y_SW, GPIO_IN);
-//     gpio_pull_up(Y_SW);
-
-//     printf("Wave Generator Ready\n");
-
-//     wave_type current_wave = WAVE_OFF;
-//     wave_output(current_wave);
-    
-//     // Initializing scaling steps and initial scales
-//     float x_scale = 1.0f;
-//     float y_scale = 1.0f;
-//     float x_offset = 0.0f;
-//     float y_offset = 0.0f;
-
-//     float x_step = 0.3f;
-//     float y_step = 0.3f;
-
-//     bool x_mode = false;   // false for scale, true for shift
-//     bool y_mode = false;   // same as above
-//     // loop functions
-
-//     while (1) {
-//         // For the push buttons
-//         if (check_mode_button()) {
-//         // if (gpio_get(BUTTON_MODE)) {
-//             current_wave = (current_wave + 1) % WAVE_COUNT;
-//             wave_output(current_wave);
-//         }
-
-//         if (check_capture_button()) {
-//             printf("Successfully Captured Waveform! :D\n");
-//         }
-
-//         // For the rotary encoder
-//         // to check mode of the buttons
-//         if (check_x_button()) {
-//             x_mode = !x_mode;
-//             int x_check_mode = x_mode ? "Shift" : "Scale";
-//             printf("X mode: %s\n", x_check_mode);
-//         }
-
-//         if (check_y_button()) {
-//             y_mode = !y_mode;
-//             int y_check_mode = y_mode ? "Shift" : "Scale";
-//             printf("Y mode: %s\n", y_check_mode);
-//         }
-
-//         // check what mode it is and scale or shift accordingly
-//         int x_check_turn = read_encoder_x();
-//         if (x_check_turn != 0) {
-//             if (x_mode) {
-//                 if (x_check_turn > 0) {
-//                     x_offset += x_step;
-//                 } else {
-//                     x_offset -= x_step;
-//                 }
-//                 printf("X offset = %.2f\n", x_offset);
-
-//             } else {
-//                 if (x_check_turn > 0) {
-//                     x_scale += x_step;
-//                 } else {
-//                     x_scale -= x_step;
-//                     if (x_scale < 0.1f) x_scale = 0.1f;
-//                 }
-//                 printf("X scale = %.2f\n", x_scale);
-//             }
-//         }
-
-//         int y_check_turn = read_encoder_y();
-//         if (y_check_turn != 0) {
-//             if (y_mode) {
-//                 if (y_check_turn > 0) {
-//                     y_offset += y_step;
-//                 } else {
-//                     y_offset -= y_step;
-//                 }
-//                 printf("Y offset = %.2f\n", y_offset);
-                
-//             } else {
-//                 if (y_check_turn > 0) {
-//                     y_scale += y_step;
-//                 } else {
-//                     y_scale -= y_step;
-//                     if (y_scale < 0.1f) y_scale = 0.1f;
-//                 }
-//                 printf("Y scale = %.2f\n", y_scale);
-//             }
-//         }
-
-//         sleep_ms(10);
+// bool check_mode_button()
+// {
+//     bool current_state = gpio_get(BUTTON_MODE);
+//     if (!last_mode_state && current_state)
+//     {
+//         last_mode_state = current_state;
+//         sleep_ms(1000);
+//         return true;
 //     }
+//     last_mode_state = current_state;
+//     return false;
 // }
+
+// bool check_capture_button(){
+//     int current_state = gpio_get(BUTTON_CAPTURE);
+//     if (!last_capture_state && current_state)
+//     {
+//         last_capture_state = current_state;
+//         return true;
+//     }
+//     last_capture_state = current_state;
+//     return false;
+// }
+
+// int read_encoder_x()
+// {
+//     static int last_clk_x = 0;
+//     int clk = gpio_get(X_CLK);
+//     int dt = gpio_get(X_DT);
+//     int result = 0;
+
+//     if (clk != last_clk_x)
+//     {                                  // pulse happended
+//         result = (dt != clk) ? 1 : -1; // 1 = clockwise
+//     }
+//     last_clk_x = clk;
+//     return result;
+// }
+// int read_encoder_y()
+// {
+//     static int last_clk_y = 1;
+//     int clk = gpio_get(Y_CLK);
+//     int dt = gpio_get(Y_DT);
+//     int result = 0;
+//     if (clk != last_clk_y)
+//     {   
+//         result = (dt != clk) ? 1 : -1; // pulse happended
+//     }
+//     last_clk_y = clk;
+//     return result;
+// }
+
+// bool check_x_button()
+// {
+//     bool current = gpio_get(X_SW);
+
+//     if (!last_x_state && current)
+//     {
+//         sleep_ms(100);
+//         last_x_state = current;
+//         return true;
+//     }
+//     last_x_state = current;
+//     return false;
+// }
+
+
+// bool check_y_button()
+// {
+//     bool current = gpio_get(Y_SW);
+
+//     if (!last_y_state && current)
+//     {
+//         sleep_ms(100);
+//         last_y_state = current;
+//         return true;
+//     }
+//     last_y_state = current;
+//     return false;
+// }
+
+// // void wave_output(wave_type wave)
+// // {
+// //     switch (wave)
+// //     {
+// //     case WAVE_OFF:
+// //         printf("Wave OFF\n");
+// //         break;
+// //     case WAVE_SINE:
+// //         printf("Wave SINE\n");
+// //         break;
+// //     case WAVE_SAWTOOTH:
+// //         printf("Wave SAW\n");
+// //         break;
+// //     case WAVE_TRIANGLE:
+// //         printf("Wave TRIANGLE\n");
+// //         break;
+// //     case WAVE_SQUARE:
+// //         printf("Wave SQUARE\n");
+// //         break;
+// //     default:
+// //         break;
+// //     }
+// // }
+
+
+
+
+
+// // read the x scale encoder
+// // int read_encoder_x() {
+// //     static int last_clk = 1;
+// //     int clk = gpio_get(X_CLK);
+
+// //     if (clk != last_clk && clk == 0) {  // detect falling edge
+// //         if (gpio_get(X_DT) == 1)
+// //             return +1;   // clockwise
+// //         else
+// //             return -1;   // counter-clockwise
+// //     }
+// //     last_clk = clk;
+// //     return 0;
+// // }
+
+
+
+// // y scale encoder reading
+// // int read_encoder_y()
+// // {
+// //     static int last_clk = 1;
+// //     int clk = gpio_get(Y_CLK);
+
+// //     if (clk != last_clk && clk == 0)
+// //     { // detect falling edge
+// //         if (gpio_get(Y_DT) == 1)
+// //             return +1; // clockwise
+// //         else
+// //             return -1; // counter-clockwise
+// //     }
+// //     last_clk = clk;
+// //     return 0;
+// // }
+
+// // check when x rotray button is pressed
+
+
+
+// // this is check when y button is pressed or not
+
+// int x_mode = 0;  // 0 = scale, 1 = shift
+// int y_mode = 0;
+
+// void controls()
+// {
+//     static int current_wave = 0;
+//     extern int volts_per_div;
+//     extern int time_per_div;
+//     extern float dt;
+//     extern bool grid_dirty;
+//     extern bool pause;
+
+//     float x_step = 0.3;
+//     float y_step = 0.3;
+
+//     float x_offset = 0;
+//     float y_offset = 0;
+
+
+//     //wave_output(current_wave);
+//     // Initializing scaling steps and initial scales
+//     // float x_scale = 1.0f;
+//     // float y_scale = 1.0f;
+//     // float x_offset = 0.0f;
+//     // float y_offset = 0.0f;
+
+//     // For the push buttons
+//     if (check_mode_button())
+//     {
+//         // if (gpio_get(BUTTON_MODE)) {
+//         current_wave = (current_wave + 1) % WAVE_COUNT;
+//         printf("Wave changed to %d\n", current_wave);
+//     }
+
+//     if (check_capture_button())
+//     {
+//         printf("Successfully Captured Waveform! :D\n");
+//     }
+//     // For the rotary encoder
+//     // to check mode of the buttons
+//     if (check_x_button())
+//     {
+//         x_mode = !x_mode;
+//         char *x_check_mode = x_mode ? "Shift" : "Scale";
+//         printf("X mode: %s\n", x_check_mode);
+//     }
+
+//     if (check_y_button())
+//     {
+//         y_mode = !y_mode;
+//         char *y_check_mode = y_mode ? "Shift" : "Scale";
+//         printf("Y mode: %s\n", y_check_mode);
+//     }
+
+//     // check what mode it is and scale or shift accordingly
+//     int x_check_turn = read_encoder_x();
+//     int y_check_turn = read_encoder_y();
+//     if (x_check_turn != 0)
+//     {
+//         if (x_mode)
+//         { // 1 = shift, 0 = scale
+//             if (x_check_turn > 0)
+//             {
+//                 x_offset += x_step;
+//             }
+//             else
+//             {
+//                 x_offset -= x_step;
+//             }
+//             printf("X offset = %.2f\n", x_offset);
+//         }
+//         else{
+//             if (x_check_turn > 0){
+//                 time_per_div++;
+//             }
+//             else{
+//                 time_per_div--;
+//                 if (time_per_div < 1){
+//                     time_per_div = 1;
+//                 }
+//             }
+//             printf("Time div = %dms\n", time_per_div);
+//             dt = time_per_div * 0.001;
+//             grid_dirty = true;
+//         }
+//     }
+
+//     if (y_check_turn != 0){
+//         if (y_mode){
+//             if (y_check_turn > 0){
+//                 y_offset += y_step;
+//             }
+//             else{
+//                 y_offset -= y_step;
+//             }
+//             printf("Y offset = %.2f\n", y_offset);
+//         }
+//         else{
+//             if (y_check_turn > 0){
+//                 volts_per_div++;
+//             }
+//             else{
+//                 volts_per_div--;
+//                 if (y_scale < 1){
+//                     volts_per_div = 1;
+//                 }
+//             }
+//             printf("Y scale = %dV\n", volts_per_div);
+//             grid_dirty = true;
+//         }
+//     }
+//     // sleep_ms(10);
+// }
+
+// ---- Encoder Counts (from interrupt) ----
+volatile int x_encoder_steps = 0;
+volatile int y_encoder_steps = 0;
+
+// ---- Encoder Modes ----
+int x_mode = 0;  // 0 = scale, 1 = shift
+int y_mode = 0;
+bool x_mode_changed = false;
+bool y_mode_changed = false;
+
+
+// ---- Shift and Scale Variables ----
+float x_offset = 0;
+float y_offset = 0;
+
+extern int volts_per_div;
+extern int time_per_div;
+extern float dt;
+extern bool grid_dirty;
+
+void encoder_callback(uint gpio, uint32_t events)
+{
+    if (gpio == X_CLK)
+    {
+        int clk = gpio_get(X_CLK);
+        int dt  = gpio_get(X_DT);
+        x_encoder_steps += (dt != clk) ? 1 : -1;
+        x_encoder_steps %= 10;
+    }
+    else if (gpio == Y_CLK)
+    {
+        int clk = gpio_get(Y_CLK);
+        int dt  = gpio_get(Y_DT);
+        y_encoder_steps += (dt != clk) ? 1 : -1;
+    }
+}
+
+void init_encoders()
+{
+    // X encoder
+    gpio_init(X_CLK);
+    gpio_set_dir(X_CLK, GPIO_IN);
+    gpio_pull_up(X_CLK);
+
+    gpio_init(X_DT);
+    gpio_set_dir(X_DT, GPIO_IN);
+    gpio_pull_up(X_DT);
+
+    // Y encoder
+    gpio_init(Y_CLK);
+    gpio_set_dir(Y_CLK, GPIO_IN);
+    gpio_pull_up(Y_CLK);
+
+    gpio_init(Y_DT);
+    gpio_set_dir(Y_DT, GPIO_IN);
+    gpio_pull_up(Y_DT);
+
+    gpio_set_irq_enabled_with_callback(
+    X_CLK,
+    GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+    true,
+    &encoder_callback
+    );
+
+    gpio_set_irq_enabled(
+        Y_CLK,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true
+    );
+
+}
+
+bool check_button(int pin, bool *last_state)
+{
+    bool s = gpio_get(pin);
+    if (s && !(*last_state))
+    {
+        sleep_ms(20);  // debounce
+        *last_state = true;
+        return true;
+    }
+    if (!s)
+        *last_state = false;
+
+    return false;
+}
+
+bool last_x_button = false;
+bool last_y_button = false;
+bool last_mode_button = false;
+bool last_capture_button = false;
+
+void controls()
+{
+    // ---- Handle X Button (toggle mode) ----
+    if (check_button(X_SW, &last_x_button))
+    {
+        x_mode = !x_mode;
+        x_mode_changed = true;
+        printf("X Mode: %s\n", x_mode ? "Shift" : "Scale");
+    }
+
+    // ---- Handle Y Button (toggle mode) ----
+    if (check_button(Y_SW, &last_y_button))
+    {
+        y_mode = !y_mode;
+        y_mode_changed = true;
+        printf("Y Mode: %s\n", y_mode ? "Shift" : "Scale");
+    }
+
+    // ---- MODE button (wave change) ----
+    if (check_button(BUTTON_MODE, &last_mode_button)){
+        printf("Mode button pressed\n");
+    }
+    // ---- CAPTURE button ----
+    if (check_button(BUTTON_CAPTURE, &last_capture_button)){
+        printf("Capture pressed\n");
+    }
+
+    // ---- Consume X encoder steps ----
+    int steps_x = x_encoder_steps;
+    x_encoder_steps = 0;
+
+    if (steps_x != 0)
+    {
+        if (x_mode == 1)        // SHIFT
+        {
+            x_offset += steps_x * 0.3f;
+            printf("X Offset = %.2f\n", x_offset);
+            grid_dirty = true;
+        }
+        else                    // SCALE (time)
+        {
+            time_per_div += steps_x;
+            time_per_div %= 11;
+            if (time_per_div < 1) time_per_div = 1;
+
+            dt = time_per_div * 0.001; // second
+            grid_dirty = true;
+
+            printf("Time/div = %dms\n", time_per_div);
+        }
+    }
+
+    // ---- Consume Y encoder steps ----
+    int steps_y = y_encoder_steps;
+    y_encoder_steps = 0;
+
+    if (steps_y != 0)
+    {
+        if (y_mode == 1)       // SHIFT
+        {
+            y_offset = y_offset + steps_y * volts_per_div;
+            grid_dirty = true;
+            printf("Y Offset = %.2f\n", y_offset);
+        }
+        else                   // SCALE (voltage)
+        {
+            volts_per_div += steps_y;
+            volts_per_div %= 11;
+            if (volts_per_div < 1) volts_per_div = 1;
+
+            grid_dirty = true;
+            printf("Volts/div = %dV\n", volts_per_div);
+        }
+    }
+}
--- CODE/src/main.c
+++ CODE/src/main.c
@@ -3,8 +3,12 @@
 #include "lcd.h"
 #include <stdio.h>
 #include <string.h>
-#include <math.h>   
+#include <math.h>  
+#define _USE_MATH_DEFINES 
 #include "wavegen.h"
+#include "buttons.h"
+
+
 
 
 #define BUFFER_SIZE 4096
@@ -48,67 +52,123 @@ void init_spi_lcd() {
     spi_set_format(spi1, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 }
 
+int volts_per_div = 1;
+int time_per_div = 1;
+int pixels_per_div = 20; //20-LCH-20
+float dt = 0.001; // Xms/div
+bool grid_dirty = false;
+bool pause = false;
+extern int x_mode;
+extern int y_mode;
 //grid and general display for buttons
-void draw_grid(int volts_per_div, int pixels_per_div) {
+void draw_grid() {
     LCD_Clear(BACKGROUND);
 
     // draw grids
     for (int xx = 0; xx < LCD_W; xx++) {
-        for (int yy = pixels_per_div; yy < LCD_H-pixels_per_div+1; yy++) {
-            if ((xx % pixels_per_div == 0 || yy % pixels_per_div == 0)) {
-                LCD_DrawPoint(xx, yy, GRID_COLOR);
+        for (int yy = 20; yy < LCD_H-20+1; yy++) {
+            if ((xx % 20 == 0 || yy % 20 == 0)) {
+                LCD_DrawPoint(xx+12, yy, GRID_COLOR);
             }
-            if ((yy== 160)){
-                LCD_DrawPoint(xx, yy, VIRTUAL_0);
+            if ((yy== 160-(y_offset*pixels_per_div))){
+                LCD_DrawPoint(xx+12, yy%(LCD_H), VIRTUAL_0);
             }
         }
     }
     char bufVolt[20];
     char bufTime[20];
     sprintf(bufVolt, "%dV/DIV", volts_per_div);
-    sprintf(bufTime, "%dms/DIV", pixels_per_div);
+    sprintf(bufTime, "%dms/DIV", time_per_div);
     LCD_DrawString(20, 5, 0xF800, BACKGROUND, bufVolt, 12, 1);
-    LCD_DrawString(LCD_W-80, 5, 0xF800, BACKGROUND, bufTime, 12, 1);
-    LCD_DrawString(10, LCD_H - 15, 0xF800, BACKGROUND, "Time Scale", 12, 1);
+    LCD_DrawString(LCD_W-60, 5, 0xF800, BACKGROUND, bufTime, 12, 1);
+    
+    //ALL labels stay the same, only virtual 0 line moves to show how much is offsetted
+    char buf[8];
+    for (int i = 0; i <= LCD_H/2-20; i += pixels_per_div){
+        int label_voltage = volts_per_div * (i / pixels_per_div);
+        printf("label: %d\n", label_voltage);
+        sprintf(buf, "%d", label_voltage);
+        LCD_DrawString(0, (LCD_H/2) - i-5, 0xF800, BACKGROUND, buf, 12, 1);
+        LCD_DrawString(0, (LCD_H/2) + i-5, 0xF800, BACKGROUND, buf, 12, 1);
+    }
+    for (int i = 10; i <= LCD_W-30; i += pixels_per_div){
+        int label_time = time_per_div * ((i-10) / pixels_per_div);
+        printf("label: %d\n", label_time);
+        sprintf(buf, "%d", label_time);
+        LCD_DrawString(i, LCD_H - 15, 0xF800, BACKGROUND, buf, 12, 1);
+    }
 }
 float get_sample(float t) { // SAMPLE SIGNAL 
     // You can replace this with ADC input later. 
-    return 2 * sin(10*t); // 2 Hz wave + 0.25f * sinf(2 * 3.14159f * 10.0f * t) // 10 Hz wave ; }
+    return 2 * sin(2* 3.14159f *100*t); // 2 Hz wave + 0.25f * sinf(2 * 3.14159f * 10.0f * t) // 10 Hz wave ; }
 }
+void display_mode_message(const char *msg) {
+    LCD_DrawFillRectangle(LCD_W/2-50,5,LCD_W/2+40,19,BACKGROUND);
+    LCD_DrawString(LCD_W/2-20, 5, 0xF800, BACKGROUND, msg, 12, 1);
+}
+
 #ifdef SINETEST
-void run_oscilloscope(int volts_per_div, int pixels_per_div) {
-    draw_grid(volts_per_div, pixels_per_div);
+void run_oscilloscope() {
+    draw_grid();
 
     int x = 0;
     float t = 0;
-    float dt = 0.01;
     float volt1 = 0;
     float volt2 = 0;
     int old_x =0;
     float pixel1[LCD_W]={0};
     float pixel2[LCD_W]={0};
-    int pixel_per_volt = pixels_per_div/volts_per_div;
+    int pixel_per_volt = 0;
+    int pixel_per_period = 0;
 
     while (1) {
-         for (int yy = pixels_per_div; yy < LCD_H - pixels_per_div+1; yy++) {
-            if ((x % pixels_per_div == 0 || yy % pixels_per_div == 0)) {
-                LCD_DrawPoint(x, yy, GRID_COLOR);
+        controls();
+        if (x_mode_changed) {
+            x_mode_changed = false;
+
+            char msg[16];
+            sprintf(msg, "%s X", x_mode ? "Shift" : "Scale");
+            display_mode_message(msg);
+        }
+
+        if (y_mode_changed) {
+                y_mode_changed = false;
+
+                char msg[16];
+                sprintf(msg, "%s Y", y_mode ? "Shift" : "Scale");
+                display_mode_message(msg);
+        }
+
+        pixel_per_volt = pixels_per_div/volts_per_div;
+        float time_per_pixel = (float)time_per_div / pixels_per_div;
+        if (grid_dirty) {
+            draw_grid();
+            grid_dirty = false;
+            char msg[16];
+            sprintf(msg, "%s X", x_mode ? "Shift" : "Scale");
+            display_mode_message(msg);
+            sprintf(msg, "%s Y", y_mode ? "Shift" : "Scale");
+            display_mode_message(msg);
+        }
+         for (int yy = 20; yy < LCD_H - 20+1; yy++) {
+            if ((x % 20 == 0 || yy % 20 == 0)) {
+                LCD_DrawPoint(x+12, yy, GRID_COLOR);
             }
             else{
-                LCD_DrawPoint(x, yy, BACKGROUND);
+                LCD_DrawPoint(x+12, yy, BACKGROUND);
             }
-            if ((yy== 160)){
-                LCD_DrawPoint(x, yy, VIRTUAL_0);
+            if ((yy== 160-y_offset*pixels_per_div)){
+                LCD_DrawPoint(x+12, yy, VIRTUAL_0);
             }
         }
         // sample the signal
         volt1 = get_sample(t);
-        t += dt;
+        t += time_per_pixel * 0.001; //t += dt;
         volt2 = get_sample(t);
 
         // convert to vertical pixel
-        pixel1[x] = ((LCD_H) / 2) - volt1 * pixel_per_volt;
-        pixel2[x] = ((LCD_H) / 2) - volt2 * pixel_per_volt;
+        pixel1[x] = ((LCD_H) / 2) - volt1 * pixel_per_volt - y_offset*pixels_per_div;
+        pixel2[x] = ((LCD_H) / 2) - volt2* pixel_per_volt - y_offset*pixels_per_div;
 
         if (pixel1[x]<pixels_per_div){
             pixel1[x] = pixels_per_div;
@@ -122,14 +182,12 @@ void run_oscilloscope(int volts_per_div, int pixels_per_div) {
         if (pixel2[x]>LCD_H-pixels_per_div){
             pixel2[x] = LCD_H-pixels_per_div;
         }
-
         // draw line
         old_x = (x==0)? 0:x-1; // %240, always wrap around after reaching end of screen
-        LCD_DrawLine(old_x,pixel1[old_x],x,pixel2[x],TRACE_COLOR);
+        LCD_DrawLine(old_x+12,pixel1[old_x],x+12,pixel2[x],TRACE_COLOR);
 
-        x = (x+1) % (LCD_W-1); // %240, always wrap around after reaching end of screen
-
-        sleep_ms(10);
+        x = ((x+1) % (LCD_W)); // %240, always wrap around after reaching end of screen
+        sleep_ms(2);
     }
 }
 #else
@@ -185,13 +243,14 @@ void run_oscilloscope() {
 
 int main() {
     stdio_init_all();
-
+    init_gpio();
     init_spi_lcd();
     init_adc_dma();
     LCD_Setup();
     LCD_Clear(BACKGROUND);
-    int volts_per_div = 1;
-    int pixels_per_div = 80;
-    run_oscilloscope(volts_per_div, pixels_per_div);
+    init_encoders();   // NEW
+    run_oscilloscope();
+    
+
     while (1);
 }
