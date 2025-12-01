7d9d3a172fbc4d2d8d6662ffd6b400c67e06a5be
David Nguyen <nguye916@purdue.edu>
2025-12-01 13:11:57 -0500

    I fixed some syntax errors and modified some codes in wavegen.c and wavegen.h

--- CODE/include/wavegen.h
+++ CODE/include/wavegen.h
@@ -1,7 +1,9 @@
 #ifndef WAVEGEN_H
 #define WAVEGEN_H
 
-#include<stdint.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "pico/stdlib.h"
 //#include "PrintCore/stdlib.h"
 
 typedef enum {
@@ -14,20 +16,21 @@ typedef enum {
 } WaveType;
 // Wavefrom generator configuration structure
 typedef struct {
-    WaveType;           //Current waveform type
+    WaveType type;      //Current waveform type
     float frequency;    //Frequency in Hz
     float amplitude;    //Amplitude (0v to 3.3v)
     bool enabled;       //Enable/Disable
 } WaveGenConfig;
 
-extern WaveGenCongif wavegen_congfig;
+extern WaveGenConfig wavegen_config;
 
-void wavegen_init(pin);
+void wavegen_init(uint32_t pin);
 void wavegen_set_type(WaveType type);
 void wavegen_set_frequency(float freq);
 void wavegen_set_amplitude(float amp);
 void wavegen_enable(bool enable);
 void wavegen_update(void);
+float wavegen_get_sample(float t);
 
 
 #endif 
--- CODE/src/wavegen.c
+++ CODE/src/wavegen.c
@@ -36,6 +36,38 @@ static inline float clampf(float x, float lo, float hi){
     return x;
 }
 
+// (from David) This is needed because the oscilloscope can't read the PWM, it's a hardware thingy. 
+// It's not checking for the voltage sample
+float wavegen_get_sample(float t)
+{
+    if (!wavegen_config.enabled)
+        return 0.0f;
+
+    float amplitude = wavegen_config.amplitude;
+    float freq = wavegen_config.frequency;
+
+    float phase = freq * t;
+
+    switch (wavegen_config.type)
+    {
+        case WAVE_SINE:
+            return amplitude * sinf(2.0f * M_PI * phase);
+
+        case WAVE_SQUARE:
+            return (sinf(2.0f * M_PI * phase) > 0 ? amplitude : -amplitude);
+
+        case WAVE_SAWTOOTH:
+            return amplitude * (2.0f * (phase - floorf(phase + 0.5f)));
+
+        case WAVE_TRIANGLE:
+            return amplitude * (2.0f * fabsf(2.0f * (phase - floorf(phase + 0.5f))) - 1.0f);
+
+        default:
+            return 0.0f;
+    }
+}
+
+
 static void wavegen_fill_wavetable(WaveType, float amp_volt)
 {
     float norm_amp = clampf(amp_volt, 0.0f, WAVEGEN_MAX_VOLT);
@@ -71,7 +103,7 @@ static void wavegen_fill_wavetable(WaveType, float amp_volt)
                 if (t < 0.25f) //first quarter: ramp up from 0 to ~1
                 s = 4.0f * t;
                 else if (t < 0.74f)
-                s = 2.0f - 4.0f * 1; // middle seg
+                s = 2.0f - 4.0f * t; // middle seg
                 else
                 s = 4.0f * t - 4.0f; // final seg : ramp from negative region up toward
                 break;
